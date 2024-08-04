015dbaa85326871dcee473828ddf30b20d3406b2
wasimhistory <wasimhistory@gmail.com>
2024-08-04 20:21:43 +0530

    first git project

---
+++ webui.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import functions
+
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is to improve productivity")
+
+todos = functions.get_todos()
+
+for index, todo in enumerate(todos):
+    check_box = st.checkbox(todo, key=todo)
+    if check_box:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(label="", placeholder="Add New todo", on_change=add_todo, key="new_todo")
+#st.session_state
+
+#print("Hello")
